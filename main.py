b2ed6493fd93eba1fe00a03dd90306119e94c4dd
geen <angelinedias53@gmail.com>
2025-05-28 00:31:43 +0530

    data ingestion done

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from src.mlProject import logger
+from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    pipeline = DataIngestionPipeline()
+    pipeline.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 
 logger.info("Logging has been set up successfully.")
